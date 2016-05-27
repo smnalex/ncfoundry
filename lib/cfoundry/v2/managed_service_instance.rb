00456b82adef2b6d3ea0e1012880b9227139ef44
Hirotaka Okura <hirotaka.okura.vy@hitachi-solutions.com>
2016-05-27 16:58:55 +0900

    Add an attribute for the latest Service Broker API

--- lib/cfoundry/v2/managed_service_instance.rb
+++ lib/cfoundry/v2/managed_service_instance.rb
@@ -3,6 +3,7 @@ require "cfoundry/v2/service_instance"
 module CFoundry::V2
   class ManagedServiceInstance < ServiceInstance
     attribute :credentials, :hash
+    attribute :parameters, :hash
     to_one    :service_plan
 
     def self.object_name
